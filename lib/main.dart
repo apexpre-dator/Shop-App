ab08a1cbce8a0ec1b2db903c70b32065cb9e6075
apexpre-dator <96236927+apexpre-dator@users.noreply.github.com>
2022-04-13 15:04:25 +0530

    Adding Providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import './screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
+import './providers/products.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,14 +14,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shop App',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.red,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'Shop App',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.red,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverViewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverViewScreen(),
     );
   }
 }

092e4a82d2824a0604c8f0b6f3d1747ea9216fcf
apexpre-dator <96236927+apexpre-dator@users.noreply.github.com>
2022-04-14 10:57:37 +0530

    Updating listener

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -45,4 +45,8 @@ class Products with ChangeNotifier {
   void addProduct() {
     notifyListeners();
   }
+
+  Product findById(String id) {
+    return _items.firstWhere((prod) => prod.id == id);
+  }
 }

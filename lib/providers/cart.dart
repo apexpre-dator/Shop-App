68ab7a3041e768ef8aad57fae1d0fc8c0dc80d36
apexpre-dator <96236927+apexpre-dator@users.noreply.github.com>
2022-04-14 17:37:10 +0530

    Adding Cart model

---
+++ lib/providers/cart.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class CartItem {
+  final String id, title;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    required this.id,
+    required this.price,
+    required this.quantity,
+    required this.title,
+  });
+}
+
+class Cart with ChangeNotifier {
+  late Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(
+    String productId,
+    double price,
+    String title,
+  ) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+        productId,
+        (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          price: existingCartItem.price,
+          quantity: existingCartItem.quantity + 1,
+          title: existingCartItem.title,
+        ),
+      );
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+          id: DateTime.now().toString(),
+          price: price,
+          quantity: 1,
+          title: title,
+        ),
+      );
+    }
+  }
+}

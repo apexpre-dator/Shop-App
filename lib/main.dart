08cda032d20d7e65dd0fc6b9130258cda61f4cc5
apexpre-dator <96236927+apexpre-dator@users.noreply.github.com>
2022-04-15 13:36:00 +0530

    Adding orders provider

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import './screens/products_overview_screen.dart';
 import './screens/cart_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
+import './providers/orders.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,6 +24,9 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider(
           create: (ctx) => Cart(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Orders(),
         ),
       ],
       child: MaterialApp(

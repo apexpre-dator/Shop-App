ce0d431f909f133ef209b374ebb19851f0ada177
apexpre-dator <96236927+apexpre-dator@users.noreply.github.com>
2022-04-15 10:56:42 +0530

    Deleting Cart Items

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -58,6 +58,7 @@ class CartScreen extends StatelessWidget {
               itemCount: cart.itemCount,
               itemBuilder: (ctx, i) => CartItem(
                 cart.items.values.toList()[i].id,
+                cart.items.keys.toList()[i],
                 cart.items.values.toList()[i].title,
                 cart.items.values.toList()[i].price,
                 cart.items.values.toList()[i].quantity,
--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,33 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/cart.dart';
 
 class CartItem extends StatelessWidget {
-  final String id, title;
+  final String id, productId, title;
   final double price;
   final int quantity;
 
-  CartItem(this.id, this.title, this.price, this.quantity);
+  CartItem(
+    this.id,
+    this.productId,
+    this.title,
+    this.price,
+    this.quantity,
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 5,
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 35,
+        ),
+        alignment: Alignment.centerLeft,
+        padding: EdgeInsets.only(left: 20),
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 5,
+        ),
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Padding(
-              padding: const EdgeInsets.all(4),
-              child: FittedBox(
-                child: Text('\$$price'),
+      direction: DismissDirection.startToEnd,
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
+      },
+      child: Card(
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 5,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: const EdgeInsets.all(4),
+                child: FittedBox(
+                  child: Text('\$$price'),
+                ),
               ),
             ),
+            title: Text(title),
+            subtitle: Text('Total: \$${price * quantity}'),
+            trailing: Text('$quantity x'),
           ),
-          title: Text(title),
-          subtitle: Text('Total: \$${price * quantity}'),
-          trailing: Text('$quantity x'),
         ),
       ),
     );

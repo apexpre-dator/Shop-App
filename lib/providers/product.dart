0e2fbd1b9321d6e7bc9ab896d6e0c810ea1dd747
apexpre-dator <96236927+apexpre-dator@users.noreply.github.com>
2022-04-21 16:07:47 +0530

    Updating favourite status

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id, title, description, imageUrl;
@@ -14,8 +17,24 @@ class Product with ChangeNotifier {
     this.isFavourite = false,
   });
 
-  void toggleFavourite() {
+  Future<void> toggleFavourite() async {
+    final prev = isFavourite;
     isFavourite = !isFavourite;
     notifyListeners();
+    final url = Uri.parse(
+        'https://shop-app-f8428-default-rtdb.asia-southeast1.firebasedatabase.app/products/$id.json');
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode({'isFavourite': isFavourite}),
+      );
+      if (response.statusCode >= 400) {
+        isFavourite = prev;
+        notifyListeners();
+      }
+    } catch (error) {
+      isFavourite = prev;
+      notifyListeners();
+    }
   }
 }

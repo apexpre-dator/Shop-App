63c9d26a77d334478d463809b3f54d58b86b840e
apexpre-dator <96236927+apexpre-dator@users.noreply.github.com>
2022-04-23 17:02:50 +0530

    Login

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -7,9 +7,10 @@ class Auth with ChangeNotifier {
   late String _token, _userId;
   late DateTime _expiryDate;
 
-  Future<void> signup(String email, String password) async {
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
     final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB5H2aTBy2ubO6RHcqgse0U98rf8iTpj6U');
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyB5H2aTBy2ubO6RHcqgse0U98rf8iTpj6U');
     final response = await http.post(
       url,
       body: json.encode({
@@ -19,5 +20,13 @@ class Auth with ChangeNotifier {
       }),
     );
     print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }

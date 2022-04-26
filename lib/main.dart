1ebfdd5bdb9934e612876d0eece3cf59a6e6bf9a
apexpre-dator <96236927+apexpre-dator@users.noreply.github.com>
2022-04-26 16:14:58 +0530

    Custom Route Transitions

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
 import './providers/auth.dart';
+import './models/custom_route.dart';
 
 void main() {
   runApp(const MyApp());
@@ -55,6 +56,12 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.purple,
             accentColor: Colors.red,
             fontFamily: 'Lato',
+            pageTransitionsTheme: PageTransitionsTheme(
+              builders: {
+                TargetPlatform.android: CustomPageTransitionBuilder(),
+                TargetPlatform.iOS: CustomPageTransitionBuilder(),
+              },
+            ),
           ),
           home: auth.isAuth
               ? const ProductsOverViewScreen()
